52bcda7dd9b584e13e8bc8e93f41cf75c75cf12f
Alexander Panyukov <olenevodec@gmail.com>
2019-06-22 00:02:01 +0300

    Квадрат двигается

--- src/App.tsx
+++ src/App.tsx
@@ -6,18 +6,92 @@ import {
   SafeAreaView,
   Platform
 } from 'react-native';
+import Animated from 'react-native-reanimated';
+import { PanGestureHandler, State } from 'react-native-gesture-handler';
+
+const { Value, event, cond, eq, set, add } = Animated;
 
 export class App extends React.Component {
+  private translateX = new Value(0);
+  private translateY = new Value(0);
+
+  private animationState = new Value(State.UNDETERMINED);
+
+  private onGestureEvent = event([
+    {
+      nativeEvent: {
+        translationX: this.translateX,
+        translationY: this.translateY,
+        state: this.animationState
+      }
+    }
+  ]);
+
   render() {
+    const translateX = interaction(this.translateX, this.animationState);
+    const translateY = interaction(this.translateY, this.animationState);
+
     return (
       <SafeAreaView style={styles.container}>
-        <Box />
+        <PanGestureHandler
+          onHandlerStateChange={this.onGestureEvent}
+          onGestureEvent={this.onGestureEvent}
+        >
+          <Animated.View
+            style={[
+              styles.box,
+              {
+                transform: [{ translateX, translateY }]
+              }
+            ]}
+          />
+        </PanGestureHandler>
       </SafeAreaView>
     );
   }
 }
 
-const Box: React.FC<{}> = () => <View style={styles.box} />;
+function interaction(
+  translate: Animated.Value<number>,
+  state: Animated.Value<number>
+): any {
+  const start = new Value(0);
+  const dragging = new Value(0);
+  const position = new Value(0);
+
+  return cond(
+    eq(state, State.ACTIVE),
+    [
+      cond(eq(dragging, 0), [set(dragging, 1), set(start, position)]),
+      set(position, add(start, translate))
+    ],
+    [set(dragging, 0), position]
+  );
+}
+
+/**
+ * 
+ * 
+function interaction() {
+ let dragging = 0;
+ let start = 0;
+ let position = 0;
+
+ return (gestureTranslation, gestureState) => {
+   if (gestureState === State.ACTIVE) {
+     if (dragging === 0) {
+       dragging = 1;
+       start = position;
+     }
+     position = start + gestureTranslation;
+   } else {
+     dragging = 0;
+   }
+   return position;
+ };
+}
+
+ */
 
 const styles = StyleSheet.create({
   container: {
